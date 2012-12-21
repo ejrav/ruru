77d7d650210cdfec68506ef03c4be3a1171c4b10
ervis jace <ervis.jace@gmail.com>
2012-12-21 23:52:12 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruru::Application.config.secret_token = '644646b2d399478fb61de8596c68d9b1ac61f9c89bd8c70a822fba8dbef735a546d2e232fe0ca8aa577aa8289418f188cf51240923c4b07826840b83cb7232a5'
